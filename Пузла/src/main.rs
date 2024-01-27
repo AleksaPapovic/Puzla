c6991d0a4ec713dd76d27a44842c8fbc6e97391a
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2024-01-27 18:52:59 +0100

    feat: delovi slika i niz bajtova za novu sliku

--- Пузла/src/main.rs
+++ Пузла/src/main.rs
@@ -1,128 +1,137 @@
 use std::{borrow::Borrow};
-use image::{io::Reader as ImageReader, DynamicImage, GenericImageView, ImageBuffer, RgbImage};
+use image::{imageops::crop_imm, io::Reader as ImageReader, DynamicImage, EncodableLayout, GenericImage, GenericImageView, ImageBuffer, Pixel, Rgb, RgbImage, RgbaImage};
 use egui::{CentralPanel, Window, Label};
 
+
 fn main() {
-    let img = ImageReader::open("src/examples/picture1.jpg")
+    let mut img = ImageReader::open("src/examples/picture5.jpg")
         .unwrap()
         .decode()
         .unwrap();
 
-
     let img_bytes = img.to_bytes();
-    createImage(img,&mut img_bytes.clone());
+    create_image(img,&mut img_bytes.clone());
 
-    let native_options = eframe::NativeOptions::default();
-    // eframe::run_native(
-    //     "Image Bytes Display",
-    //     native_options,
-    //     Box::new(|_cc| Box::new(MyApp::new(img_bytes))),
-    // );
-
-
-    let imgx = ImageReader::open("src/examples/slika 1/part x.jpg")
-    .unwrap()
-    .decode()
-    .unwrap();
-let imgx_bytes = imgx.to_bytes();
-createImage(imgx,&mut imgx_bytes.clone());
-let native_options2 = eframe::NativeOptions::default();
-eframe::run_native(
-    "Image Bytes Display2",
-    native_options2,
-    Box::new(|_cc| Box::new(MyApp::new(imgx_bytes))),
-);
+    // open_image(img_bytes);
 
 }
 
+fn open_image(img_bytes:Vec<u8>){
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Image Bytes Display",
+        native_options,
+        Box::new(|_cc| Box::new(MyApp::new(img_bytes))),
+    );
+}
 
-fn createImage(img: DynamicImage,bytes: &mut Vec<u8>){
+fn create_image(img: DynamicImage,bytes: &mut Vec<u8>){
     for i in 0.. bytes.len() {
         bytes[i]= 0
     }
-    let img2 = ImageReader::open("src/examples/slika 1/part b.jpg")
+    let img2 = ImageReader::open("src/examples/slika 5/26.jpg")
     .unwrap()
     .decode()
     .unwrap();
 
-    compare(&mut bytes.clone(),img.clone(),img2.clone());
+    compare(&mut bytes.clone(),&mut img.clone(),&mut img2.clone());
     
-    appendPart(&mut bytes.clone(),img,img2);
+    append_part(&mut bytes.clone(),&mut img.clone(),img2);
 }
 
-fn compare(bytes: &mut Vec<u8>,img: DynamicImage,part_img: DynamicImage){
+fn sum_of_bytes(bytes: &[u8]) -> u32 {
+    bytes.iter().map(|&b| b as u32).sum()
+}
 
-    let ow = img.width();
-    let oh = img.height();
+fn compare(bytes: &mut Vec<u8>,img: &mut DynamicImage,part_img:  &mut DynamicImage){
+    let img_width = img.width();
+    let img_height = img.height();
     let partw = part_img.width();
     let parth = part_img.height();
-    let diffw = ow-partw;
-    let bytes_per_pixel = 3;
 
+    let mut part_number = 1;
 
-    let x = ow / partw;
-    let y = oh / parth;
+    let cols = img_width / partw;
+    let rows = ((img_height as f64) / (parth as f64)).round() as u32;
 
-    let mut image_index = 1;
+    for y in 0..rows {
+        for x in 0..cols {
+            let top_left_x = x * partw;
+            let top_left_y = y * parth;
 
-    for x in 0..x {
-        for y in 0..y {
-            let mut img_section = ImageBuffer::new(partw, parth);
+            let part_img = img.crop_imm(top_left_x, top_left_y, partw, parth);
+            let filename = format!("output{}.png", y * cols + x + 1);
 
-            // Copy pixels from the main image to the section
-            for i in 0..partw {
-                for j in 0..parth {
-                    let pixel = img.get_pixel(x * partw + i, y * parth + j);
-                    img_section.put_pixel(i, j, pixel);
-                }
-            }
-
-            let file_name = format!("output{}.jpg", image_index);
-            img_section.save(file_name).expect("Failed to save image");
-            image_index += 1;
+            part_img.save(filename);
         }
     }
 }
 
 
-fn appendPart(bytes: &mut Vec<u8>,img: DynamicImage, part_img: DynamicImage){
+fn append_part(bytes: &mut Vec<u8>,img: &mut DynamicImage, part_img: DynamicImage){
+        let ow = (img.width()*3) as usize;
+        let oh = (img.height()*3) as usize;
+        println!("Original image size: {}x{}", ow, oh);
+    
+        let part_width = (part_img.width()*3) as usize;
+        let part_height = (part_img.height()*3) as usize;
+        println!("Part image size: {}x{}", part_width, part_height);
+    
+        let x_parts = ow / part_width;
+        let y_parts = ((oh as f64) / (part_height as f64)).round() as usize;
+        println!("Number of parts: {}x{}", x_parts, y_parts);
+    
+        let diff = ( ow %part_width);
+        println!("diff {}",diff);
+        let img1: RgbImage = ImageBuffer::from_raw(img.width(), img.height(), bytes.clone()).expect("test");
+        let part_bytes = part_img.to_bytes();
+        let mut start_index=0;
+        for y in 0..x_parts {
+            for x in 0..y_parts {
+                start_index = (x * part_width);
+                let mut end_index = start_index + part_width;
+                println!("{}",start_index);
+                println!("{}",end_index);
+                if(x ==0){
 
-    let ow = img.width() as usize;
-    let oh = img.height() as usize;
-    println!("{}",ow);
-    println!("{}",oh);
-    let partw = part_img.width() as usize;
-    let parth = part_img.height() as usize;
-    println!("{}",partw);
-    println!("{}",parth);
-
-    let diffw = ow-partw;
-    println!("{}",diffw);
-    let bytes_per_pixel = 3;
-
-
-    let x = ow / partw;
-    let y = oh / parth;
-
-    let bytes_per_pixel = 3;
-
-    let start_index = 123;
-    let part_bytes = part_img.to_bytes();
-
-for j in 0..parth {
-    let src_row_start = j * partw * bytes_per_pixel;
-    let dest_row_start = start_index + j * (partw + diffw) * bytes_per_pixel;
-
-    if dest_row_start + partw * bytes_per_pixel <= bytes.len() {
-        bytes[dest_row_start..dest_row_start + partw * bytes_per_pixel]
-            .copy_from_slice(&part_bytes[src_row_start..src_row_start + partw * bytes_per_pixel]);
+                    for p in 0..part_height/3{
+                        // println!("s{}",start_index);
+                        // println!("e{}",end_index);
+                        // println!("ps{}",p*part_width);
+                        // println!("pe{}",p*part_width +part_width);
+                        bytes[start_index..end_index].copy_from_slice(&part_bytes[p*part_width..(p*part_width +part_width)]);
+                        start_index += ow;
+                        end_index = start_index + part_width;
+                    } 
+                }
+        }
     }
-}
+    
+    let img: RgbImage = ImageBuffer::from_raw(img.width() as u32, img.height() as u32, bytes.clone()).expect("test");
+    img.save("oput.jpg").unwrap();
+    let parts_across = x_parts as u32;
+    let parts_down = y_parts as u32;
+    let w =  (part_width/3) as u32;
+    let h =  (part_height/3) as u32;
 
-    let img: RgbImage = ImageBuffer::from_raw(img.width(), img.height(), bytes.clone())
-        .expect("greska");
+    let mut new_img = RgbaImage::new(parts_across * w, parts_down * h);
 
-    img.save("output.png").unwrap();
+    let part_pixels = part_img.to_rgba8();
+
+    for y in 0..parts_down {
+        for x in 0..parts_across {
+            let start_x = x * w;
+            let start_y = y * h;
+            for part_y in 0..h {
+                for part_x in 0..w {
+                    let pixel = part_pixels.get_pixel(part_x, part_y);
+                    new_img.put_pixel(start_x + part_x, start_y + part_y, *pixel);
+                }
+            }
+        }
+    }
+
+    new_img.save("oput_pixelbypixel.jpg").unwrap();
 }
 
 fn cutOriginalImage(){
