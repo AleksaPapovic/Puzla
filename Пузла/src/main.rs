a5fa10d337f4e7442982a8f00d18a6c895318bc4
AleksaPapovic <papovic.ra167.2018@uns.ac.rs>
2024-01-28 13:34:42 +0100

    feat: mne compare

--- Пузла/src/main.rs
+++ Пузла/src/main.rs
@@ -17,8 +17,11 @@ use image::{
 };
 
 fn main() {
-    let mut img = ImageReader::open("src/examples/picture1.jpg").unwrap().decode().unwrap();
-    create_image(img);
+    let redni_broj = 1;
+    let putnja_slike = format!("src/examples/picture{redni_broj}.jpg");
+    let putanja_delova = format!("src/examples/slika {redni_broj}");
+    let mut img = ImageReader::open(putnja_slike).unwrap().decode().unwrap();
+    create_image(img, putanja_delova);
 }
 
 fn read_images(path_str: String) -> Vec<DynamicImage> {
@@ -63,21 +66,34 @@ fn read_images(path_str: String) -> Vec<DynamicImage> {
     return images;
 }
 
-fn create_image(img: DynamicImage) {
+fn create_image(img: DynamicImage, putanja_delova: String) {
     let mut bytes = img.to_bytes();
     for i in 0..bytes.len() {
         bytes[i] = 0;
     }
-    let putanja = String::from("src/examples/slika 1");
-    let parts = read_images(putanja);
-    let width = parts[0].width();
-    let height: u32 = parts[0].height();
-    let original_parts = read_parts(&mut img.clone(), width, height);
+
+    let parts = read_images(putanja_delova);
     for (index, part) in parts.iter().enumerate() {
-        let diffs = compare(part, original_parts.clone(), width, height);
+        let original_parts = read_parts(&mut img.clone(), part.width(), part.height());
+        let diffs: Vec<f64> = compare(part, original_parts.clone(), part.width(), part.height());
         println!("diffs {:?}", diffs);
-        // append_part(&mut bytes.clone(), &mut img.clone(), part.clone());
+        let (indeks, val) = find_max_with_index(&diffs);
+        println!("indeks {},val {}", indeks, val);
+        // append_part(&mut bytes.clone(), &mut img.clone(), part.clone(), 10); vrati
     }
+}
+
+fn find_max_with_index(numbers: &[f64]) -> (usize, f64) {
+    let mut max_value = numbers[0];
+    let mut max_index = 0;
+
+    for (index, &value) in numbers.iter().enumerate().skip(1) {
+        if value > max_value {
+            max_value = value;
+            max_index = index;
+        }
+    }
+    return (max_index, max_value);
 }
 
 fn read_parts(img: &mut DynamicImage, partw: u32, parth: u32) -> Vec<DynamicImage> {
@@ -95,13 +111,17 @@ fn read_parts(img: &mut DynamicImage, partw: u32, parth: u32) -> Vec<DynamicImag
             let part_img = img.crop_imm(top_left_x, top_left_y, partw, parth);
             let filename = format!("output{}.png", y * cols + x + 1);
             part_img.save(filename);
+            // let mut novi_img = ImageReader::open(format!("output{}.png", y * cols + x + 1))
+            //     .unwrap()
+            //     .decode()
+            //     .unwrap();
             images.push(part_img);
         }
     }
     return images;
 }
 
-fn compare(part: &DynamicImage, originals: Vec<DynamicImage>, width: u32, height: u32) -> Vec<f32> {
+fn compare(part: &DynamicImage, originals: Vec<DynamicImage>, width: u32, height: u32) -> Vec<f64> {
     let tolerance = Tolerance {
         red: 20,
         green: 20,
@@ -121,73 +141,72 @@ fn compare_images(
     width: u32,
     height: u32,
     tolerance: &Tolerance
-) -> f32 {
-    let mut diff = 0u64;
-    let mut count = 0;
-    for y in 0..height {
-        for x in 0..width {
-            let pixel1 = img1.get_pixel(x, y);
-            let pixel2 = img2.get_pixel(x, y);
+) -> f64 {
+    println!("img1 w{}h{}", img1.width(), img1.height());
+    // println!("img2 w{}h{}", img2.width(), img2.height());
+    let mut mse = 0f64;
+    let mut count = 0usize;
+    for y in 0..img2.height() {
+        for x in 0..img2.width() {
+            let pixel1 = img1.to_rgb().to_vec();
+            let pixel2 = img2.to_rgb().to_vec();
 
-            if
-                !is_within_tolerance(pixel1[0], pixel2[0], tolerance.red) ||
-                !is_within_tolerance(pixel1[1], pixel2[1], tolerance.green) ||
-                !is_within_tolerance(pixel1[2], pixel2[2], tolerance.blue)
-            {
+            for i in 0..3 {
+                mse += (((pixel1[i] as f64) - (pixel2[i] as f64)) / 255.0).powi(2);
                 count += 1;
             }
         }
     }
-    let total_pixels = (width * height) as u64;
-    (count as f32) / (total_pixels as f32)
+
+    let diff = mse / (count as f64);
+    return diff;
 }
 
-fn is_within_tolerance(value1: u8, value2: u8, tolerance: u8) -> bool {
-    let diff = ((value1 as i16) - (value2 as i16)).abs() as u8;
-    diff <= tolerance
-}
-
-fn append_part(bytes: &mut Vec<u8>, img: &mut DynamicImage, part_img: DynamicImage) {
+fn append_part(bytes: &mut Vec<u8>, img: &mut DynamicImage, part_img: DynamicImage, indeks: usize) {
     let ow = (img.width() * 3) as usize;
     let oh = (img.height() * 3) as usize;
-    println!("Original width: {} height: {}", ow, oh);
+    // println!("Original width: {} height: {}", ow, oh);
 
     let partw = (part_img.width() * 3) as usize;
     let parth = (part_img.height() * 3) as usize;
-    println!("Part width: {} height: {}", partw, parth);
+    // println!("Part width: {} height: {}", partw, parth);
 
     let x = ow / partw;
     let y = ((oh as f64) / (parth as f64)).round() as usize;
-    println!("x: {} y: {}", x, y);
+    // println!("x: {} y: {}", x, y);
 
     let diff = ow % partw;
     let img1: RgbImage = ImageBuffer::from_raw(img.width(), img.height(), bytes.clone()).expect(
         "error"
     );
+    let mut counter = 0;
     let part_bytes = part_img.to_bytes();
     let mut start_index = 0;
     for j in 0..x {
         for i in 0..y {
+            // println!("Part i: {} j: {} counter: {}", i, j, counter);
             start_index = i * partw;
             let mut end_index = start_index + partw;
-            println!("{}", start_index);
-            println!("{}", end_index);
-            // if(x ==0){
-
-            for p in 0..parth / 3 {
-                // println!("s{}",start_index);
-                // println!("e{}",end_index);
-                // println!("ps{}",p*partw);
-                // println!("pe{}",p*partw +partw);
-                let part_index = p * partw;
-                bytes[start_index..end_index].copy_from_slice(
-                    &part_bytes[part_index..part_index + partw]
-                );
-                start_index += ow;
-                end_index = start_index + partw;
+            // println!("{}", start_index);
+            // println!("{}", end_index);
+            if counter == 6 {
+                for p in 0..parth / 3 {
+                    // println!("s{}",start_index);
+                    // println!("e{}",end_index);
+                    // println!("ps{}",p*partw);
+                    // println!("pe{}",p*partw +partw);
+                    let part_index = p * partw;
+                    let start = start_index + (((10 * partw) / 3) * parth) / 3;
+                    let end = start + partw;
+                    // println!("{},{}", start, end);
+                    bytes[start..end].copy_from_slice(&part_bytes[part_index..part_index + partw]);
+                    start_index += ow;
+                    end_index = start_index + partw;
+                }
             }
-            // }
+            counter += 1;
         }
+        counter += 1;
     }
 
     let img: RgbImage = ImageBuffer::from_raw(
